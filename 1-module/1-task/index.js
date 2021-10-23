4e84765d80937cb41d3fd33a88a86d367f35492f
foksap <foksap.mail.ru>
2021-10-23 15:42:19 +0300

    Решил  1 и 2 задачу

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,12 @@
 function factorial(n) {
-  // ваш код...
-}
+    let FirstFactorial = 1;
+    for (i=1; i<=n ; i++){
+      FirstFactorial *=i
+    }
+    return FirstFactorial;
+  }
+
+factorial(0); // 1
+factorial(1); // 1
+factorial(3); // 6
+factorial(5); // 120
