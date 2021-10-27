1f4a838c68d42cc77b7fdce210f42f88007b9b34
foksap <foksap.mail.ru>
2021-10-27 15:19:18 +0300

    Решение 4 из 5 задач

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,7 +1,12 @@
 function checkSpam(str) {
-  let bottomStr = str.toLowerCase();
-  return bottomStr.includes("1xBet") || bottomStr.includes("XXX")
-}
- checkSpam("1xBet");  
- checkSpam("XXX"); 
- checkSpam("innocent rabbit"); 
+   let bottomStr= str.toLowerCase()
+   if(bottomStr.includes("1xbet") || bottomStr.includes("xxx")){
+   return true;
+  }
+  else {
+    return false;
+  }
+} 
+(checkSpam("1xBet"));  
+(checkSpam("XXX")); 
+(checkSpam("innocent rabbit")); 
--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,10 @@
-function truncate(str, maxlength) {
-  // ваш код...
-}
+function  truncate(str, maxlength){
+  if(str.length > maxlength) {
+    str=str.substring (0, maxlength-1)+"…";
+    return str;
+ } else {
+     return str;
+ }
+}  
+truncate("Вот, что мне хотелось бы сказать на эту тему:", 20 );
+truncate('Всем привет!', 20);
