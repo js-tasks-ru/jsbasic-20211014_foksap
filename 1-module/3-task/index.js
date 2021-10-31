e054c194be51686f413ecb50cd8dc314396490cb
foksap <foksap.mail.ru>
2021-10-31 17:19:14 +0300

    1 задача обьект

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -8,4 +8,3 @@ function ucFirst(str) {
   return str2 ; 
   };    
 }
-ucFirst();
--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,21 @@
+let salaries = {
+  John:1000,
+  Ann: 1600,
+  Pete: 1300,
+  month: "December",
+  currency: 'USD',
+  isPayed: false,
+}
 function sumSalary(salaries) {
-  // ваш код...
+  let sum =0;
+  for (let key in salaries) {
+    if (isFinite(salaries[key])===false) {
+      continue;
+    }
+    else if (typeof (salaries[key] == "number")){
+      sum +=salaries[key];
+      continue;
+    }  
+  }
+  return sum;
 }
